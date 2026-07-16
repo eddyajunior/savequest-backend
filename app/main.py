869559087a1da74ab3d171deb2a7ace345d22a06
Edson Amaral <eddyajunior@gmail.com>
2026-07-16 20:35:48 -0300

    chore: bootstrap Save Quest backend

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Save Que$t API",
+        version="0.1.0",
+        description="API da Plataforma Save Que$st para gerenciamento de transações financeiras e controle de gastos.",
+        )
+
+    @app.get("/")
+    async def read_root():
+        return {"message": "Welcome to my FastAPI application!"}
+
+    return app
+
+app = create_app()
